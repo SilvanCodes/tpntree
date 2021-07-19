62c76ec97b64e5bc63d970cfed0adadffcf84665
Silvan Buedenbender <silvancodes@gmail.com>
2021-07-19 13:14:06 +0200

    Update name and description

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-//! This crate conatins an N-dimensional generalization of a quad-tree called **T**wo-**p**ower-__n__-tree or tpn-tree,
+//! This crate contains an N-dimensional generalization of a region quadtree called **T**wo-**p**ower-__n__-tree or tpntree,
 //! as there exist 2^N children per node, where N is the number of dimensions.
+//! A quadtree is the two-dimensional case, an octtree is the three-dimensional case of the tpntree.
 
 mod tpntree;
 
