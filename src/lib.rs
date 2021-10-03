837965caea9fca7938624bcf995a0e63bdb121d3
Silvan Buedenbender <silvancodes@gmail.com>
2021-10-03 23:33:36 +0200

    Add coordinate trait

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,25 @@
 //! as there exist 2^N children per node, where N is the number of dimensions.
 //! A quadtree is the two-dimensional case, an octtree is the three-dimensional case of the tpntree.
 
+mod errors;
 pub mod tpntree;
 pub mod tpntree_dynamic;
+
+pub use errors::TpnTreeError;
+
+/// [`Coordinates`] is required for a type to be used inside a [`SpatialTree`].
+pub trait Coordinates {
+    fn coordinates(&self) -> &[f64];
+}
+
+impl<const N: usize> Coordinates for [f64; N] {
+    fn coordinates(&self) -> &[f64] {
+        self
+    }
+}
+
+impl Coordinates for Vec<f64> {
+    fn coordinates(&self) -> &[f64] {
+        self
+    }
+}
